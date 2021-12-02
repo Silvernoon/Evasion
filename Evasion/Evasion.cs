3245196b72c7d7b8b6363976530e72a78e4c3892
blaxxun <blaxxun@blaxxun.blaxxun>
2021-12-02 15:55:00 +0100

    initial release

---
+++ Evasion/Evasion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SkillManager;
+
+namespace Evasion
+{
+	[BepInPlugin(ModGUID, ModName, ModVersion)]
+	public class Evasion : BaseUnityPlugin
+	{
+		private const string ModName = "Evasion";
+		private const string ModVersion = "1.0";
+		private const string ModGUID = "org.bepinex.plugins.evasion";
+
+		public void Awake()
+		{
+			Skill evasion = new("Evasion", "evasion-icon.png");
+			evasion.Description.English("Reduces the stamina usage of dodging.");
+			evasion.Name.German("Ausweichen");
+			evasion.Description.German("Reduziert die benötigte Ausdauer um auszuweichen.");
+			evasion.Configurable = true;
+			
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			Harmony harmony = new(ModGUID);
+			harmony.PatchAll(assembly);
+		}
+		
+		[HarmonyPatch(typeof(Player), nameof(Player.UpdateDodge))]
+		private class ReduceStaminaUsage
+		{
+			[UsedImplicitly]
+			private static void Prefix(Player __instance)
+			{
+				__instance.m_dodgeStaminaUsage *= 1 - __instance.GetSkillFactor("Evasion") * 0.5f;
+			}
+			
+			[UsedImplicitly]
+			private static void Postfix(Player __instance)
+			{
+				__instance.m_dodgeStaminaUsage /= 1 - __instance.GetSkillFactor("Evasion") * 0.5f;
+
+				if (__instance.InDodge() && __instance.m_queuedDodgeTimer == 0)
+				{
+					__instance.RaiseSkill("Evasion");
+				}
+			}
+		}
+	}
+}
